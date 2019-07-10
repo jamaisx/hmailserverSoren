bd5dac052d21ccc0cf330638a5edd84feba1229d
RvdHout <ruud.vd.hout@hotmail.com>
2019-07-10 23:51:15 +0200

    5.6.8-B2437.17
    - Add email address variable to SignatureAdder.cpp
    - DKIM on acccount-rule 'reply' not applied
    - preserve RewriteEnvelopeFromWhenForwarding setting when forwarding from account rule
    - The logical flow should be to disregard "Require SMTP authentication" if "Allow deliveries from" is unselected

--- hmailserver/source/Server/SMTP/RuleApplier.cpp
+++ hmailserver/source/Server/SMTP/RuleApplier.cpp
@@ -175,7 +175,7 @@ namespace HM
          }
       case RuleAction::Reply:
          {
-            ApplyAction_Reply(pAction, pMsgData);
+			ApplyAction_Reply(pAction, account, pMsgData);
             break;
          }
       case RuleAction::ScriptFunction:
@@ -247,7 +247,7 @@ namespace HM
       // We need to update the SMTP envelope from address, if this
       // message is forwarded by a user-level account.
       std::shared_ptr<CONST Account> pAccount = CacheContainer::Instance()->GetAccount(rule_account_id_);
-      if (pAccount)
+	  if (pAccount && IniFileSettings::Instance()->GetRewriteEnvelopeFromWhenForwarding())
          pMsg->SetFromAddress(pAccount->GetAddress());
       
       // Add new recipients
@@ -374,7 +374,7 @@ namespace HM
    }
 
    void 
-   RuleApplier::ApplyAction_Reply(std::shared_ptr<RuleAction> pAction, std::shared_ptr<MessageData> pMsgData) const
+   RuleApplier::ApplyAction_Reply(std::shared_ptr<RuleAction> pAction, std::shared_ptr<const Account> account, std::shared_ptr<MessageData> pMsgData) const
    {
       // true = check AutoSubmitted header and do not respond if set
       if (!IsGeneratedResponseAllowed(pMsgData, true))
@@ -394,15 +394,19 @@ namespace HM
 
       std::shared_ptr<Account> emptyAccount;
 
-      // Sen d a copy of this email.
+      // Send a copy of this email.
       std::shared_ptr<Message> pMsg = std::shared_ptr<Message>(new Message());
       pMsg->SetState(Message::Delivering);
       
       String newMessageFileName = PersistentMessage::GetFileName(pMsg);
 
+	  // check if this us a user-level account rule or global rule.
+	  std::shared_ptr<CONST Account> pAccount = CacheContainer::Instance()->GetAccount(rule_account_id_);
+
       std::shared_ptr<MessageData> pNewMsgData = std::shared_ptr<MessageData>(new MessageData());
       pNewMsgData->LoadFromMessage(newMessageFileName, pMsg);
-      pNewMsgData->SetReturnPath("");
+	  if (!pAccount)
+		  pNewMsgData->SetReturnPath("");
       pNewMsgData->GenerateMessageID();
       pNewMsgData->SetTo(sReplyRecipientAddress);
       pNewMsgData->SetFrom(pAction->GetFromName() + " <" + pAction->GetFromAddress() + ">");
@@ -412,7 +416,11 @@ namespace HM
       pNewMsgData->SetAutoReplied();
 	  pNewMsgData->IncreaseRuleLoopCount();
       pNewMsgData->Write(newMessageFileName);
-	  
+
+	  // We need to update the SMTP envelope from address, if this
+	  // message is replied to by a user-level account.
+	  if (pAccount)
+		  pMsg->SetFromAddress(pAccount->GetAddress());
 
       // Add recipients.
       bool recipientOK = false;
