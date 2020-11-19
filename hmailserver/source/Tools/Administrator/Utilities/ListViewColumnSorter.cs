1f0cf8c8ecb94667159e6e5b73bd3bb626a4b683
Søren Raagaard Rathje <8338115+SorenRR@users.noreply.github.com>
2020-11-19 09:38:13 +0100

    Update ListViewColumnSorter.cs

--- hmailserver/source/Tools/Administrator/Utilities/ListViewColumnSorter.cs
+++ hmailserver/source/Tools/Administrator/Utilities/ListViewColumnSorter.cs
@@ -198,13 +198,13 @@ public class ListViewColumnSorter : IComparer
                     for (var i = 0; i < octets; i++)
                     {
                         compareResult = xBytes[i].CompareTo(yBytes[i]);
-                        if (compareResult != 0)
 
                             if (OrderOfSort == SortOrder.Descending)
                                 compareResult = -compareResult;
 
-                        return compareResult;
-                    }
+						if (compareResult != 0)
+							return compareResult;
+					}
                     return compareResult;
                 }
             }
