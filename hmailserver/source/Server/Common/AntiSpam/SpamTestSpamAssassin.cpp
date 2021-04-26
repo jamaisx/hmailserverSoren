bf5d379a27c3e4b61e425ad8e39cf5a49779c70b
Søren Raagaard Rathje <8338115+SorenRR@users.noreply.github.com>
2021-04-26 12:23:25 +0200

    Add "Return-Path" while processing mail by SpamAssassin

--- hmailserver/source/Server/Common/AntiSpam/SpamTestSpamAssassin.cpp
+++ hmailserver/source/Server/Common/AntiSpam/SpamTestSpamAssassin.cpp
@@ -66,7 +66,19 @@ namespace HM
       TraceHeaderWriter writer;
       writer.Write(sFilename, pMessage, fieldsToWrite);
 
-	  std::shared_ptr<IOService> pIOService = Application::Instance()->GetIOService();
+      // Add Return-Path header if none exist (ExternalAccount download?)
+      if (pTestData->GetMessageData()->GetReturnPath().IsEmpty())
+      {
+         String sEnvelopeFrom = pTestData->GetEnvelopeFrom();
+
+         std::vector<std::pair<AnsiString, AnsiString>> fieldsToWrite;
+         fieldsToWrite.push_back(std::make_pair("Return-Path", sEnvelopeFrom));
+
+         TraceHeaderWriter writer;
+         writer.Write(sFilename, pMessage, fieldsToWrite);
+      }
+
+      std::shared_ptr<IOService> pIOService = Application::Instance()->GetIOService();
 
       bool testCompleted;
 
@@ -115,6 +127,11 @@ namespace HM
       // Check if the message is tagged as spam.
       std::shared_ptr<MessageData> pMessageData = pTestData->GetMessageData();
       pMessageData->RefreshFromMessage();
+
+      // The Return-Path header was added above to help SpamAssassin with its SPF checks.
+      // We should remove it again to restore the headers to original state (except for any added by SA).
+      pMessageData->DeleteField("Return-Path");
+      pMessageData->Write(sFilename);
 
       bool bIsSpam = false;
       AnsiString sSpamStatus = pMessageData->GetFieldValue("X-Spam-Status");
